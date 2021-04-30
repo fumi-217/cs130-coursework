bd5a97f318249a40a7df9d509da6771d2cb90533
Fumika Hoshi <fumi@Fumikas-MacBook-Pro.local>
2021-04-30 17:33:53 -0400

    Adding my tutorial05 to the repo

---
+++ tutorial05/theme-switcher/js/index.js
@@ -0,0 +1,16 @@
+const defaultTheme = () => {
+   document.querySelector(".container").className = "container";
+};
+
+const oceanTheme = () => {
+   document.querySelector(".container").className = "container ocean";
+};
+
+const desertTheme = () => {
+   document.querySelector(".container").className = "container desert";
+};
+
+
+document.querySelector("#default").onclick = defaultTheme;
+document.querySelector("#ocean").onclick = oceanTheme;
+document.querySelector("#desert").onclick = desertTheme;
